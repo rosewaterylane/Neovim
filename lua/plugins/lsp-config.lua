0a4f51babbe11be0b9c463bfa7d86bec3abce960
Dwight <dwightrose2@icloud.com>
2024-06-26 16:49:18 -0500

    NeoVim Complete Config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "pyright", "rust_analyzer", "html", "cssls", "clangd"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({capabilities = capabilities})
+      lspconfig.tsserver.setup({capabiltities = capabilities})
+      lspconfig.pyright.setup({capabiltities = capabilities})
+      lspconfig.rust_analyzer.setup({capabilities = capabilities})
+      lspconfig.html.setup({capabilities = capabilities})
+      lspconfig.cssls.setup({capabilities = capabilities})
+      lspconfig.clangd.setup({capabilities = capabilities})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,16 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.syntax = 'on'
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+-- vim.o.smartindent = true
+vim.cmd.highlight({"Normal", "guibg=none"})
+vim.cmd.highlight({"NonText", "guibg=none"})
+vim.cmd.highlight({"Normal", "ctermbg=none"})
+vim.cmd.highlight({"NonText", "ctermbg=none"})
+
+vim.g.mapleader = " "
+
+
